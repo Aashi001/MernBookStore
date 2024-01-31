83d84a4d7270442e99ed31901864b58b0565b4c5
Aashi001 <88787259+Aashi001@users.noreply.github.com>
2024-02-01 00:09:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ dotenv.config();
 //databse config
 
 connectDB();
-const__filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
